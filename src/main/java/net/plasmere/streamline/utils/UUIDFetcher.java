78b5b4b4a1eff9af989d8d24a0dfdcbab1f56474
Nitrate <thesiberiantigercat@gmail.com>
2021-01-13 01:11:00 -0500

    Added a UUID Fetcher.

---
+++ src/main/java/net/plasmere/streamline/utils/UUIDFetcher.java
@@ -0,0 +1,87 @@
+package net.plasmere.streamline.utils;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.plasmere.streamline.StreamLine;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.file.Path;
+import java.util.Objects;
+import java.util.UUID;
+
+public class UUIDFetcher {
+    static public UUID fetch(String username) throws IOException {
+        try {
+            String JSONString = "";
+
+            HttpURLConnection connection = (HttpURLConnection) new URL("https://api.mojang.com/users/profiles/minecraft/" + username).openConnection();
+            InputStream is = connection.getInputStream();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                JSONString = line;
+            }
+
+            Object obj = new JsonParser().parse(JSONString);
+            JsonObject jo = (JsonObject) obj;
+
+            String id = jo.get("id").getAsString();
+
+            // String uuid = id.substring(0, 7) + "-" + id.substring(8, 11) + "-"
+            //        + id.substring(12, 15) + "-" + id.substring(16, 19) + "-"
+            //        + id.substring(20);
+            String uuid = formatToUUID(id);
+
+            return UUID.fromString(uuid);
+            //return UUID.fromString(id);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String formatToUUID(String unformatted){
+        StringBuilder formatted = new StringBuilder();
+        int i = 1;
+        for (Character character : unformatted.toCharArray()){
+            if (i == 9 || i == 13 || i == 17 || i == 21){
+                formatted.append("-").append(character);
+            } else {
+                formatted.append(character);
+            }
+            i++;
+        }
+
+        return formatted.toString();
+    }
+
+    public static ProxiedPlayer getProxiedPlayer(UUID uuid){
+        try {
+            return StreamLine.getInstance().getProxy().getPlayer(uuid);
+        } catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static ProxiedPlayer getProxiedPlayer(String name){
+        ProxyServer proxy = StreamLine.getInstance().getProxy();
+        try {
+            ProxiedPlayer other = Objects.requireNonNull(proxy).getPlayer(UUIDFetcher.fetch(name));
+
+            if (other != null) {
+                return other;
+            }
+
+            throw new Exception("No player found!");
+        } catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
